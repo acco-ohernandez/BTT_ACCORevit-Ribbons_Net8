982d716913a04c72e331cd8fdd71d2424fd48d52
Orlando R Hernandez <47466479+acco-ohernandez@users.noreply.github.com>
2024-08-22 16:22:49 -0700

    Added Deployers for project 01,02,03 and added all the template files and images to the solution.

---
+++ RevitRibbon_MainSourceCode/Templates/App_Template_Tab.cs
@@ -0,0 +1,49 @@
+﻿// Version 1.3.0 2023-06-20
+#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+using RevitRibbon_MainSourceCode;
+#endregion
+
+namespace RevitRibbon_MainSourceCode
+{
+    class App_Template_Tab : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+
+        {
+            //// Create ribbon
+            //string assembly_Dll_File = @"C:\ACCORevit\ACCO\ACCORevit ADDINS\02-ACCORevit Ribbons\NAME_OF_FOLDER_FOR_DLLs_and_.Ribbon\RevitRibbon_MainSourceCode.dll";
+            //string xml_Ribbon_File_Path = @"C:\ACCORevit\ACCO\ACCORevit ADDINS\02-ACCORevit Ribbons\NAME_OF_FOLDER_FOR_DLLs_and_.Ribbon";
+
+            //or
+
+            // Get the path of the executing assembly (DLL)
+            string assembly_Dll_File = Assembly.GetExecutingAssembly().Location;
+
+            // Get the directory path of the assembly
+            string xml_Ribbon_File_Path = Path.GetDirectoryName(assembly_Dll_File);
+
+            // call the RibbonBuilder Class and pass it the path of the dll and *.ribbon path.
+            RibbonBuilder.build_ribbon(application, assembly_Dll_File, xml_Ribbon_File_Path);
+
+            return Result.Succeeded;
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // Do nothing
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitRibbon_MainSourceCode/Templates/Cmd_Template.cs
@@ -0,0 +1,44 @@
+﻿// Version 1.0.0 2023-02-28
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+
+// Added Revit External Libraries
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace RevitRibbon_MainSourceCode
+{
+    // Transaction required when using IExternalCommand for Revit
+    [Transaction(TransactionMode.Manual)] // Starts a new transaction manually for every process that modifies the Revit database
+    [Regeneration(RegenerationOption.Manual)]
+    public class Cmd_Template : IExternalCommand // Implementing IExternalCommand gets Revit to recognize our plugins
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Autodesk.Revit.ApplicationServices.Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            try
+            {
+                // Code here
+
+                return Result.Succeeded;
+            }
+            catch (Exception ex) // Catch errors
+            {
+                TaskDialog exceptStatus = new TaskDialog(ex.GetType().FullName.ToString());
+                exceptStatus.MainInstruction = ex.Message;
+                exceptStatus.Show();
+
+                return Result.Failed;
+            }
+
+        }
+
+    }
+}
